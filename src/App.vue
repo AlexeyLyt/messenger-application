8e9191a77bb8ced73199c013dc63768a96f4e261
AlexeyLyt <alelytov@gmail.com>
2020-02-28 17:08:54 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-main-wrapper />
   </div>
 </template>
 
+<script>
+import vMainWrapper from './components/v-main-wrapper'
+
+export default {
+  name: 'App',
+  components: { vMainWrapper }
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/contacts/v-contact-list.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="v-contact-list">
+        <v-contact-user />
+    </div>
+</template>
+
+<script>
+import vContactUser from "./v-contact-user"
+
+export default {
+    name: "v-contact-list",
+    props: {},
+    components: { vContactUser },
+    data() {
+        return {}
+    },
+    computed: {}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
